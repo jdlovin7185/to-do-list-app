498cf7290c1a00a7326c89cb91028b10dc21fe97
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-02-11 09:07:25 -0500

    Finishes converting part 1 into jquery

--- js/script.js
+++ js/script.js
@@ -9,8 +9,7 @@ function newItem(){
    if (inputValue === '') {
      alert("You must write something!");
    } else {
-     let list = document.querySelector('#list');
-     list.appendChild(li);
+     $('#list').append(li);
    }
 
  //2. Crossing out an item from the list of items:
